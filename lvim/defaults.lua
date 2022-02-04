6cb35e61cfc741275fafc70e118840c1a327b2b4
Judah Ben-Eliezer <judahbeneliezer@gmail.com>
2022-02-04 13:36:45 -0500

    changed lvim cp to ln

--- lvim/defaults.lua
+++ lvim/defaults.lua
@@ -7,7 +7,7 @@ an executable
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save = true
-lvim.colorscheme = "nightfox"
+lvim.colorscheme = "gruvbox"
 lvim.leader = "space"
 
 -- keymappings [view all the defaults by pressing <leader>Lk]
