236225ddf2c0c0efb5af7e807a912fe3b25cb291
jbeneliezer <judahbeneliezer@gmail.com>
2022-04-17 16:09:02 -0400

    ...

--- lvim/defaults.lua
+++ lvim/defaults.lua
@@ -6,13 +6,13 @@ an executable
 
 -- general
 lvim.log.level = "warn"
-lvim.format_on_save = false
+lvim.format_on_save = true
 lvim.colorscheme = "nightfox"
 lvim.leader = "space"
 
 -- keymappings [view all the defaults by pressing <leader>Lk]
--- unmap a default keymapping
 -- lvim.keys.normal_mode["<c-s>"] = ":w<cr>"
+-- unmap a default keymapping
 -- lvim.keys.normal_mode["<c-Up>"] = false
 -- edit a default keymapping
 -- lvim.keys.normal_mode["<c-q>"] = ":q<cr>"
@@ -62,26 +62,179 @@ lvim.builtin.telescope.defaults.mappings = {
 -- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<cr>", "Projects" }
 -- Use which-key to add extra bindings with the leader-key prefix
 
-lvim.builtin.which_key.vmappings["/"] = { "<ESC><cmd>lua require('Comment.api').toggle_linewise_op(vim.fn.visualmode())<cr>", "Comment" }
-lvim.builtin.which_key.vmappings["j"] = { ":m '>+1<cr>gv=gv", "Move Down" }
-lvim.builtin.which_key.vmappings["k"] = { ":m '>-2<cr>gv=gv", "Move Up" }
+lvim.builtin.which_key.vmappings = {
+	["/"] = { "<ESC><cmd>lua require('Comment.api').toggle_linewise_op(vim.fn.visualmode())<cr>", "Comment" },
+	["j"] = { ":m '>+1<cr>gv=gv", "Move Down" },
+	["k"] = { ":m '>-2<cr>gv=gv", "Move Up" },
+}
 
-lvim.builtin.which_key.mappings[";"] = { "<cmd>Dashboard<cr>", "Dashboard" }
-lvim.builtin.which_key.mappings["/"] = { "<cmd>lua require('Comment.api').toggle_current_linewise()<cr>", "Comment" }
-lvim.builtin.which_key.mappings["w"] = { "<cmd>w<cr>", "Save" }
-lvim.builtin.which_key.mappings["q"] = { "<cmd>q<cr>", "Quit" }
-lvim.builtin.which_key.mappings["x"] = { "<cmd>x<cr>", "Save and Quit" }
-lvim.builtin.which_key.mappings["!"] = { "<cmd>q!<cr>", "Force Quit" }
-lvim.builtin.which_key.mappings["c"] = { "<cmd>bd!<cr>", "Close Buffer" }
-lvim.builtin.which_key.mappings["n"] = { "<cmd>ene ", "New File" }
-lvim.builtin.which_key.mappings["s"] = { ":w ", "Save as" }
-lvim.builtin.which_key.mappings["e"] = { "<cmd>NvimTreeToggle<cr>", "Explorer" }
-lvim.builtin.which_key.mappings["o"] = { "<c-w>o", "Fullscreen" }
-lvim.builtin.which_key.mappings["j"] = { "<cmd>m .+1<cr>", "which_key_ignore" }
-lvim.builtin.which_key.mappings["a"] = { "G=gg2<c-o>zz", "Style" }
-lvim.builtin.which_key.mappings["k"] = { "<cmd>m .-2<cr>", "which_key_ignore" }
-lvim.builtin.which_key.mappings["<leader>"] = { "hl", "which_key_ignore" }
-lvim.builtin.which_key.mappings["f"] = {
+lvim.builtin.which_key.mappings = {
+	[";"] = { "<cmd>Dashboard<cr>", "Dashboard" },
+	["/"] = { "<cmd>lua require('Comment.api').toggle_current_linewise()<cr>", "Comment" },
+	["w"] = { "<cmd>w<cr>", "Save" },
+	["q"] = { "<cmd>q<cr>", "Quit" },
+	["x"] = { "<cmd>x<cr>", "Save and Quit" },
+	["!"] = { "<cmd>q!<cr>", "Force Quit" },
+	["c"] = { "<cmd>BufferClose!<cr>", "Close Buffer" },
+	["s"] = { ":w ", "Save as" },
+	["n"] = { "<cmd>ene ", "New File" },
+	["e"] = { "<cmd>NvimTreeToggle<cr>", "Explorer" },
+	["o"] = { "<c-w>o", "Fullscreen" },
+	["a"] = { "G=gg2<c-o>zz", "Style" },
+	["j"] = { "<cmd>m .+1<cr>", "which_key_ignore" },
+	["k"] = { "<cmd>m .-2<cr>", "which_key_ignore" },
+	["<leader>"] = { "hl", "which_key_ignore" },
+	b = {
+		name = "Buffers",
+		j = { "<cmd>BufferPick<cr>", "Jump" },
+		f = { "<cmd>Telescope buffers<cr>", "Find" },
+		b = { "<cmd>b#<cr>", "Previous" },
+		w = { "<cmd>BufferWipeout<cr>", "Wipeout" },
+		e = {
+			"<cmd>BufferCloseAllButCurrent<cr>",
+			"Close all but current",
+		},
+		h = { "<cmd>BufferCloseBuffersLeft<cr>", "Close all to the left" },
+		l = {
+			"<cmd>BufferCloseBuffersRight<cr>",
+			"Close all to the right",
+		},
+		D = {
+			"<cmd>BufferOrderByDirectory<cr>",
+			"Sort by directory",
+		},
+		L = {
+			"<cmd>BufferOrderByLanguage<cr>",
+			"Sort by language",
+		},
+	},
+	p = {
+		name = "Packer",
+		c = { "<cmd>PackerCompile<cr>", "Compile" },
+		i = { "<cmd>PackerInstall<cr>", "Install" },
+		r = { "<cmd>lua require('lvim.plugin-loader').recompile()<cr>", "Re-compile" },
+		s = { "<cmd>PackerSync<cr>", "Sync" },
+		S = { "<cmd>PackerStatus<cr>", "Status" },
+		u = { "<cmd>PackerUpdate<cr>", "Update" },
+	},
+
+	-- " Available Debug Adapters:
+	-- "   https://microsoft.github.io/debug-adapter-protocol/implementors/adapters/
+	-- " Adapter configuration and installation instructions:
+	-- "   https://github.com/mfussenegger/nvim-dap/wiki/Debug-Adapter-installation
+	-- " Debug Adapter protocol:
+	-- "   https://microsoft.github.io/debug-adapter-protocol/
+	-- " Debugging
+	g = {
+		name = "Git",
+		j = { "<cmd>lua require 'gitsigns'.next_hunk()<cr>", "Next Hunk" },
+		k = { "<cmd>lua require 'gitsigns'.prev_hunk()<cr>", "Prev Hunk" },
+		l = { "<cmd>lua require 'gitsigns'.blame_line()<cr>", "Blame" },
+		p = { "<cmd>lua require 'gitsigns'.preview_hunk()<cr>", "Preview Hunk" },
+		r = { "<cmd>lua require 'gitsigns'.reset_hunk()<cr>", "Reset Hunk" },
+		R = { "<cmd>lua require 'gitsigns'.reset_buffer()<cr>", "Reset Buffer" },
+		s = { "<cmd>lua require 'gitsigns'.stage_hunk()<cr>", "Stage Hunk" },
+		u = {
+			"<cmd>lua require 'gitsigns'.undo_stage_hunk()<cr>",
+			"Undo Stage Hunk",
+		},
+		o = { "<cmd>Telescope git_status<cr>", "Open changed file" },
+		b = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
+		c = { "<cmd>Telescope git_commits<cr>", "Checkout commit" },
+		C = {
+			"<cmd>Telescope git_bcommits<cr>",
+			"Checkout commit(for current file)",
+		},
+		d = {
+			"<cmd>Gitsigns diffthis HEAD<cr>",
+			"Git Diff",
+		},
+	},
+
+	l = {
+		name = "LSP",
+		a = { "<cmd>lua require('lvim.core.telescope').code_actions()<cr>", "Code Action" },
+		d = { "<cmd>Telescope diagnostics bufnr=0 theme=get_ivy<cr>", "Buffer Diagnostics" },
+		w = { "<cmd>Telescope diagnostics<cr>", "Diagnostics" },
+		f = { "<cmd>lua vim.lsp.buf.formatting()<cr>", "Format" },
+		i = { "<cmd>LspInfo<cr>", "Info" },
+		I = { "<cmd>LspInstallInfo<cr>", "Installer Info" },
+		j = {
+			"<cmd>lua vim.diagnostic.goto_next()<cr>",
+			"Next Diagnostic",
+		},
+		k = {
+			"<cmd>lua vim.diagnostic.goto_prev()<cr>",
+			"Prev Diagnostic",
+		},
+		l = { "<cmd>lua vim.lsp.codelens.run()<cr>", "CodeLens Action" },
+		p = {
+			name = "Peek",
+			d = { "<cmd>lua require('lvim.lsp.peek').Peek('definition')<cr>", "Definition" },
+			t = { "<cmd>lua require('lvim.lsp.peek').Peek('typeDefinition')<cr>", "Type Definition" },
+			i = { "<cmd>lua require('lvim.lsp.peek').Peek('implementation')<cr>", "Implementation" },
+		},
+		q = { "<cmd>lua vim.diagnostic.setloclist()<cr>", "Quickfix" },
+		r = { "<cmd>lua vim.lsp.buf.rename()<cr>", "Rename" },
+		s = { "<cmd>Telescope lsp_document_symbols<cr>", "Document Symbols" },
+		S = {
+			"<cmd>Telescope lsp_dynamic_workspace_symbols<cr>",
+			"Workspace Symbols",
+		},
+	},
+	L = {
+		name = "LunarVim",
+		c = {
+			"<cmd>edit " .. get_config_dir() .. "/config.lua<cr>",
+			"Edit config.lua",
+		},
+		f = {
+			"<cmd>lua require('lvim.core.telescope.custom-finders').find_lunarvim_files()<cr>",
+			"Find LunarVim files",
+		},
+		g = {
+			"<cmd>lua require('lvim.core.telescope.custom-finders').grep_lunarvim_files()<cr>",
+			"Grep LunarVim files",
+		},
+		k = { "<cmd>Telescope keymaps<cr>", "View LunarVim's keymappings" },
+		i = {
+			"<cmd>lua require('lvim.core.info').toggle_popup(vim.bo.filetype)<cr>",
+			"Toggle LunarVim Info",
+		},
+		I = {
+			"<cmd>lua require('lvim.core.telescope.custom-finders').view_lunarvim_changelog()<cr>",
+			"View LunarVim's changelog",
+		},
+		l = {
+			name = "logs",
+			d = {
+				"<cmd>lua require('lvim.core.terminal').toggle_log_view(require('lvim.core.log').get_path())<cr>",
+				"view default log",
+			},
+			D = {
+				"<cmd>lua vim.fn.execute('edit ' .. require('lvim.core.log').get_path())<cr>",
+				"Open the default logfile",
+			},
+			l = {
+				"<cmd>lua require('lvim.core.terminal').toggle_log_view(vim.lsp.get_log_path())<cr>",
+				"view lsp log",
+			},
+			L = { "<cmd>lua vim.fn.execute('edit ' .. vim.lsp.get_log_path())<cr>", "Open the LSP logfile" },
+			n = {
+				"<cmd>lua require('lvim.core.terminal').toggle_log_view(os.getenv('NVIM_LOG_FILE'))<cr>",
+				"view neovim log",
+			},
+			N = { "<cmd>edit $NVIM_LOG_FILE<cr>", "Open the Neovim logfile" },
+			p = {
+				"<cmd>lua require('lvim.core.terminal').toggle_log_view('packer.nvim')<cr>",
+				"view packer log",
+			},
+			P = { "<cmd>exe 'edit '.stdpath('cache').'/packer.nvim.log'<cr>", "Open the Packer logfile" },
+		},
+		r = { "<cmd>LvimReload<cr>", "Reload LunarVim's configuration" },
+		u = { "<cmd>LvimUpdate<cr>", "Update LunarVim" },
+	},
+	f = {
 		name = "Telescope",
 		f = { "<cmd>Telescope find_files<cr>", "Find File" },
 		g = { "<cmd>Telescope live_grep<cr>", "Text" },
@@ -97,8 +250,12 @@ lvim.builtin.which_key.mappings["f"] = {
 			"<cmd>lua require('telescope.builtin.internal').colorscheme({enable_preview = true})<cr>",
 			"Colorscheme with Preview",
 		},
-	}
-lvim.builtin.which_key.mappings["t"] = {
+	},
+	T = {
+		name = "Treesitter",
+		i = { ":TSConfigInfo<cr>", "Info" },
+	},
+	t = {
 		name = "Trouble",
 		r = { "<cmd>Trouble lsp_references<cr>", "References" },
 		f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
@@ -106,32 +263,32 @@ lvim.builtin.which_key.mappings["t"] = {
 		q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
 		l = { "<cmd>Trouble loclist<cr>", "LocationList" },
 		w = { "<cmd>Trouble lsp_workspace_diagnostics<cr>", "Diagnostics" },
-	}
-lvim.builtin.which_key.mappings["h"] = {
+	},
+	h = {
 		name = "Harpoon",
-		h = { "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", "Menu"},
+		h = { "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", "Menu" },
 		a = { "<cmd>lua require('harpoon.mark').add_file()<cr>", "Add" },
-	}
-lvim.builtin.which_key.mappings["1"] = { "<cmd>lua require('harpoon.ui').nav_file(1)<cr>", "which_key_ignore" }
-lvim.builtin.which_key.mappings["2"] = { "<cmd>lua require('harpoon.ui').nav_file(2)<cr>", "which_key_ignore" }
-lvim.builtin.which_key.mappings["3"] = { "<cmd>lua require('harpoon.ui').nav_file(3)<cr>", "which_key_ignore" }
-lvim.builtin.which_key.mappings["4"] = { "<cmd>lua require('harpoon.ui').nav_file(4)<cr>", "which_key_ignore" }
-lvim.builtin.which_key.mappings["5"] = { "<cmd>lua require('harpoon.ui').nav_file(5)<cr>", "which_key_ignore" }
-lvim.builtin.which_key.mappings["6"] = { "<cmd>lua require('harpoon.ui').nav_file(6)<cr>", "which_key_ignore" }
-lvim.builtin.which_key.mappings["7"] = { "<cmd>lua require('harpoon.ui').nav_file(7)<cr>", "which_key_ignore" }
-lvim.builtin.which_key.mappings["8"] = { "<cmd>lua require('harpoon.ui').nav_file(8)<cr>", "which_key_ignore" }
-lvim.builtin.which_key.mappings["9"] = { "<cmd>lua require('harpoon.ui').nav_file(9)<cr>", "which_key_ignore" }
-lvim.builtin.which_key.mappings["0"] = { "<cmd>lua require('harpoon.ui').nav_file(10)<cr>", "which_key_ignore" }
+	},
+	["1"] = { "<cmd>lua require('harpoon.ui').nav_file(1)<cr>", "which_key_ignore" },
+	["2"] = { "<cmd>lua require('harpoon.ui').nav_file(2)<cr>", "which_key_ignore" },
+	["3"] = { "<cmd>lua require('harpoon.ui').nav_file(3)<cr>", "which_key_ignore" },
+	["4"] = { "<cmd>lua require('harpoon.ui').nav_file(4)<cr>", "which_key_ignore" },
+	["5"] = { "<cmd>lua require('harpoon.ui').nav_file(5)<cr>", "which_key_ignore" },
+	["6"] = { "<cmd>lua require('harpoon.ui').nav_file(6)<cr>", "which_key_ignore" },
+	["7"] = { "<cmd>lua require('harpoon.ui').nav_file(7)<cr>", "which_key_ignore" },
+	["8"] = { "<cmd>lua require('harpoon.ui').nav_file(8)<cr>", "which_key_ignore" },
+	["9"] = { "<cmd>lua require('harpoon.ui').nav_file(9)<cr>", "which_key_ignore" },
+	["0"] = { "<cmd>lua require('harpoon.ui').nav_file(10)<cr>", "which_key_ignore" },
+}
 
 -- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
-lvim.builtin.dashboard.active = true
+-- lvim.builtin.alpha = true
 lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
-lvim.builtin.nvimtree.quit_on_open = 1
 lvim.builtin.nvimtree.show_icons.git = 0
--- lvim.builtin.nvimtree.hijack_netrw = true
+-- lvim.builtin.nvimtree.quit_on_open = 1
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
@@ -150,8 +307,8 @@ lvim.builtin.treesitter.ensure_installed = {
 	"markdown",
 }
 
+lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
--- lvim.builtin.treesitter.ignore_install = { "haskell" }
 
 -- generic LSP settings
 
@@ -213,24 +370,24 @@ lvim.builtin.treesitter.highlight.enabled = true
 -- Additional Plugins
 lvim.plugins = {
 	-- Themes
-	{"folke/tokyonight.nvim"},
-	{'morhetz/gruvbox'},
-	{'tomasr/molokai'},
-	{'sainnhe/sonokai'},
-	{'sainnhe/everforest'},
-	{'jnurmine/Zenburn'},
-	{'jacoborus/tender.vim'},
-	{'mhartington/oceanic-next'},
-	{'AlessandroYorba/Alduin'},
-	{'savq/melange'},
-	{'fcpg/vim-farout'},
-	{'Mangeshrex/uwu.vim'},
-	{'Mofiqul/dracula.nvim'},
-	{'bluz71/vim-nightfly-guicolors'},
-	{'EdenEast/nightfox.nvim'},
+	{ "folke/tokyonight.nvim" },
+	{ 'morhetz/gruvbox' },
+	{ 'tomasr/molokai' },
+	{ 'sainnhe/sonokai' },
+	{ 'sainnhe/everforest' },
+	{ 'jnurmine/Zenburn' },
+	{ 'jacoborus/tender.vim' },
+	{ 'mhartington/oceanic-next' },
+	{ 'AlessandroYorba/Alduin' },
+	{ 'savq/melange' },
+	{ 'fcpg/vim-farout' },
+	{ 'Mangeshrex/uwu.vim' },
+	{ 'Mofiqul/dracula.nvim' },
+	{ 'bluz71/vim-nightfly-guicolors' },
+	{ 'EdenEast/nightfox.nvim' },
 
 	-- Other
-	{'ThePrimeagen/harpoon'},
+	{ 'ThePrimeagen/harpoon' },
 	{
 		"folke/trouble.nvim"
 		-- cmd = "TroubleToggle",
@@ -243,4 +400,15 @@ lvim.plugins = {
 -- { "BufWinEnter", "*.lua", ":normal G=gg" },
 -- }
 
-
+if vim.fn.has "wsl" == 1 then
+	vim.g.clipboard = {
+		copy = {
+			["*"] = "win32yank.exe -i --crlf",
+			["+"] = "win32yank.exe -i --crlf",
+		},
+		paste = {
+			["+"] = "win32yank.exe -o --lf",
+			["*"] = "win32yank.exe -o --lf",
+		},
+	}
+end
