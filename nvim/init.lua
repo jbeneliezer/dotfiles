f3bab0a7824a49da31eb97a3ccc34d21314ae21e
Judah Ben-Eliezer <judahbeneliezer@gmail.com>
2022-08-04 20:57:34 -0400

    updated nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,57 +1,30 @@
-require "user.keymaps"
-require "user.plugins"
-require "user.autocommands"
-require "user.colorscheme"
-require "user.cmp"
-require "user.navic"
-require "user.lsp-inlayhints"
-require "user.lsp"
-require "user.telescope"
-require "user.treesitter"
-require "user.autopairs"
-require "user.comment"
-require "user.gitsigns"
-require "user.nvim-tree"
-require "user.lualine"
-require "user.toggleterm"
-require "user.project"
-require "user.impatient"
-require "user.indentline"
-require "user.alpha"
-require "user.whichkey"
-require "user.hop"
-require "user.matchup"
-require "user.numb"
-require "user.dial"
-require "user.colorizer"
-require "user.spectre"
-require "user.zen-mode"
-require "user.neoscroll"
-require "user.todo-comments"
-require "user.bookmark"
-require "user.symbol-outline"
-require "user.git-blame"
-require "user.gist"
-require "user.gitlinker"
-require "user.notify"
-require "user.ts-context"
-require "user.registers"
-require "user.sniprun"
-require "user.functions"
-require "user.illuminate"
-require "user.dap"
-require "user.lir"
-require "user.cybu"
-require "user.winbar"
-require "user.options"
-require "user.nvim-webdev-icons"
-require "user.bfs"
-require "user.crates"
-require "user.dressing"
-require "user.tabout"
-require "user.fidget"
-require "user.browse"
-require "user.auto-session"
-require "user.jaq"
-require "user.surround"
-require "user.harpoon"
+
+vim.g.nvim_profile_path = [[/home/jb/.config/nvim/]]
+vim.opt.runtimepath:append(vim.g.nvim_profile_path)
+vim.opt.packpath = vim.g.nvim_profile_path
+
+local packer_start_path = vim.g.nvim_profile_path .. "pack/packer/start"
+local hotpot_path = packer_start_path .. "/hotpot.nvim"
+local packer_path = packer_start_path .. "/packer.nvim"
+
+if (vim.fn.empty(vim.fn.glob(hotpot_path))) > 0 then
+    print("Cloning hotpot and packer into " .. packer_start_path)
+    vim.fn.system({'mkdir', '-p', packer_start_path})
+    vim.fn.system({'git', 'clone', 'https://github.com/rktjmp/hotpot.nvim', hotpot_path})
+    vim.fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', packer_path})
+end
+
+vim.cmd [[ packadd packer.nvim ]]
+vim.cmd [[ packadd hotpot.nvim ]]
+
+local packer = require('packer')
+packer.init({
+    package_root = vim.g.nvim_profile_path .. "pack/",
+    compile_path = vim.g.nvim_profile_path .. "lua/packer_compiled.lua"
+    })
+
+require("hotpot")
+require("jb.settings")
+
+vim.g.mapleader = " "
+vim.api.nvim_set_keymap("n", "<Leader>e", ":Lexplore<cr>", { noremap = true })
